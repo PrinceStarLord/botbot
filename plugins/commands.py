f143b752328ec667b578ba384e8e77a83fdae912
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2025-04-27 20:29:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,18 +64,12 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ', url=UPDATES),
-                    InlineKeyboardButton('ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ', url=BOTS)
-                  ],[
-                    InlineKeyboardButton('ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ 1', url=GRP1),
-                    InlineKeyboardButton('ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ 2', url=GRP2)
-                  ],[
-                    InlineKeyboardButton('ꜱʜᴀʀᴇ ᴍᴇ',url=SHARE)
+                    InlineKeyboardButton('ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ', url=UPDATES)
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons) #Start Buttons Here
         await message.reply_text(
             # photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -115,20 +109,12 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
-                  ],[
-                    InlineKeyboardButton('ʙᴀᴄᴋᴜᴘ ᴄʜᴀɴɴᴇʟ', url=f'http://telegram.me/{UPDATES}'),
-                    InlineKeyboardButton('ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ', url=BOTS)
-                  ],[
-                    InlineKeyboardButton('ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ 1', url=GRP1),
-                    InlineKeyboardButton('ᴍᴏᴠɪᴇꜱ ɢʀᴏᴜᴘ 2', url=GRP2)
-                  ],[
-                    InlineKeyboardButton('ꜱʜᴀʀᴇ ᴍᴇ',url=SHARE)
+                    InlineKeyboardButton('ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ', url=f'http://telegram.me/{UPDATES}')
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
             # photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
